7fdd5d3798a3a143381604ed56f0673a82cac295
Ademola Adesanya <adesanya1ademola@gmail.com>
2022-10-04 15:05:02 +0100

    api limit increased

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -11,17 +11,17 @@ export class RecipeService {
   constructor(private http: HttpClient) { }
 
   //api endpoints
-  defaulEndpoint: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=cake&limit=20'
-  mainCourse: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=main%20course&limit=20'
-  sideDish: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=side%20dish&limit=20'
-  dessert: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=dessert&limit=20'
-  appetizer: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=appetizer&limit=20'
-  salad: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=salad&limit=20'
-  drink: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=drink&limit=20'
-  breakfast: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=breakfast&limit=20'
-  soup: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=soup&limit=20'
-  snack: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=snack&limit=20'
-  sauce: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=sauce&limit=20'
+  defaulEndpoint: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=cake&limit=50'
+  mainCourse: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=main%20course&limit=50'
+  sideDish: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=side%20dish&limit=50'
+  dessert: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=dessert&limit=50'
+  appetizer: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=appetizer&limit=50'
+  salad: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=salad&limit=50'
+  drink: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=drink&limit=50'
+  breakfast: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=breakfast&limit=50'
+  soup: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=soup&limit=50'
+  snack: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=snack&limit=50'
+  sauce: string = 'https://api.spoonacular.com/food/search?apiKey=337c6995ce284e6d9f15bfa245279bf5&query=sauce&limit=50'
 
 
   //get dishes services
